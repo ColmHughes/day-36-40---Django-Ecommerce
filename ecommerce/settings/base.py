ff7b4b80d78c84d2bb1e8bf888e76e6afa9ccc02
Colm Hughes <cjhugo1991@hotmail.com>
2018-03-20 11:00:04 +0000

    Allowed Hosts added

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_&@i)khzb%wg_#mu*3sw@ln+@%a)%tf%)wu4xsqaiit9(s0$)t'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ecommerce-colmhughes.c9users.io']
+ALLOWED_HOSTS = ['ecommerce-colmhughes.c9users.io', 'ecommerceproject2018.herokuapp.com']
 
 
 # Application definition
